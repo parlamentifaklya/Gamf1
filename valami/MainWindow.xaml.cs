66824d15071ee71aee65a91d3c2d7bbe9925e1f8
Harciholapat <harciholapat@gmail.com>
2024-02-27 11:06:46 +0100

    added txt files

--- valami/MainWindow.xaml.cs
+++ valami/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace valami
 {
@@ -23,6 +24,7 @@ namespace valami
         public MainWindow()
         {
             InitializeComponent();
+        
         }
     }
 }
