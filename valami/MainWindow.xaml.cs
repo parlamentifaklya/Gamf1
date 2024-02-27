6cb93de5eefe73a6aab7ae630ff02796b68de1fd
Sushkoo <szilagyidominik005@gmail.com>
2024-02-27 11:20:57 +0100

    a

--- valami/MainWindow.xaml.cs
+++ valami/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace valami
 {
@@ -23,6 +24,18 @@ namespace valami
         public MainWindow()
         {
             InitializeComponent();
+
+            List<string> sorok = new List<string>();
+            using (StreamReader sr = new StreamReader("szamok.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    sorok.Add(line);
+                }
+            }
+
+
         }
     }
 }
